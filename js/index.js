304c5697526730ed69a63fe4a2eeb6ef96fe00cc
Jonathan Kulak <lackadaize@msn.com>
2024-07-19 14:08:24 -0400

    userCard data populates within #user-list. initial css styles implemented.

--- js/index.js
+++ js/index.js
@@ -9,14 +9,15 @@ const githubApiUrl = "https://api.github.com/"
 function githubSearch () {
     // Variables
     const githubForm = document.getElementById("github-form")
-    let searchInput = document.getElementById("search")
-    let userList = document.getElementById("user-list")
-    let reposList = document.getElementById("repos-list")
+    const searchInput = document.getElementById("search")
+    const usersList = document.getElementById("user-list")
+    const reposList = document.getElementById("repos-list")
 
     function userSearch (e) {
         e.preventDefault();
+        usersList.innerHTML = ''
+        reposList.innerHTML = ''
         const searchUser = searchInput.value
-        console.log(searchUser)
         githubForm.reset()
 
         fetch(`${githubApiUrl}search/users?q=${searchUser}` , {
@@ -24,29 +25,47 @@ function githubSearch () {
                 'Accept': 'application/vnd.github.v3+json'
             }
         })
-        .then(r => r.json())
-        .then((users) => {
-            console.log(typeof users)
-            console.log(users)
-        })
-        .catch(error => console.log(error))
+        .then(response => response.json())
+          .then((data) => {
+            const users = data.items
+            users.forEach((user) => {
+              loadUsers(user)
+            })
+          })
+          .catch((error) => {
+            console.log(error)
+          });
+
+        function loadUsers(user) {
+            const userCard = document.createElement('li')
+            userCard.setAttribute('class', 'user-card')
+
+            const userAvatar = document.createElement('div')
+            userAvatar.setAttribute('class', 'user-avatar')
+
+            const userName = document.createElement('p')
+            userName.setAttribute('class', 'user-name')
+
+            const userUrl = document.createElement('div')
+            userUrl.setAttribute('class', 'user-url')
+
+            usersList.appendChild(userCard)
+            userCard.appendChild(userAvatar)
+            userCard.appendChild(userName)
+            userCard.appendChild(userUrl)
+
+            userAvatar.innerHTML = `<a href="${user.html_url}" target="_blank"><img src="${user.avatar_url}"/></a>`
+            userName.innerHTML = `<a href="${user.html_url}" target="_blank">${user.login}</a>`
+            userUrl.innerHTML = `<a href="${user.html_url}" target="_blank">PROFILE</a>`
+        }
     }
 
     // Event Listeners
     githubForm.addEventListener('submit', userSearch)
     // githubForm.addEventListener('submit', repoSearch)
-
-    // console.log
-    console.log(githubApiUrl)
-    console.log(githubForm)
-    console.log(searchInput)
-    console.log(userList)
-    console.log(reposList)
 }
 
 /*
-The index.html file has a form with a search input. When the form is submitted, it should take the value of the input and search GitHub for user matches using the User Search Endpoint.
-Using the results of the search, display information about the users to the page. (You might include showing their username, avatar and a link to their profile.)
 Clicking on one of these users should send a request to the User Repos Endpoint and return data about all the repositories for that user.
 Using the response from the Users Repos Endpoint, display all the repositories for that user on the page.
 */
